c2000ece766c74e5c8a0ff71d546ee0505922aee
Najeeb Ur Rehman Raja <najeebraja22.2@gmail.com>
2026-02-18 01:17:13 +0500

    feat: Implement a new Navbar component with nested dropdowns and add initial Allama Iqbal personality pages and assets.

---
+++ app/personalities/allama-iqbal/gallery/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import NotFound from '@/app/notFound/page';
+
+export default function Gallery() {
+    return (
+       <div>
+        <NotFound/>
+       </div>
+    );
+}
---
+++ app/personalities/allama-iqbal/quotes/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import NotFound from '@/app/notFound/page';
+
+export default function Biography() {
+
+    return (
+        <div>
+            <NotFound />
+        </div>
+    );
+}
